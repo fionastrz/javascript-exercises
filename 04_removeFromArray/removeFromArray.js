a75fa5d97145e609fda308df9b83aca56cb7d856
fionastrz <115540296+fionastrz@users.noreply.github.com>
2024-12-22 10:58:00 +0100

    added solutions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, item1, item2 = "", item3 = "", item4 = "") {
+    newArray="";
+    if (array.includes(item1) || array.includes(item2) || array.includes(item3) || array.includes(item4)){
+        newArray = array.filter((elements) => elements !== item1 && elements !== item2 && elements !== item3 && elements !== item4);
+    }
+    else {
+        newArray = array;
+    }
+    return newArray;        
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let result = 0;
+    if (Number.isInteger(min) && Number.isInteger(max) && min > 0 && max > 0) {
 
+        if ((min < max)) {
+            for(i = min; i<=max; i++){
+                result += i;
+            }
+            return result;
+        }
+        else {
+            return sumAll(max, min);
+        }
+    }
+    else {
+        return "ERROR";
+    }   
 };
 
 // Do not edit below this line
